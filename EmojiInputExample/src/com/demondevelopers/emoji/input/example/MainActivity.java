506d67ca2cf411af342a98d79742452a74415a0f
Oliver Hunt <oliver.huntuk@gmail.com>
2014-07-27 19:00:32 +0100

    Improved backspacing and inserting, added some stuff to hide and show keyboards

--- EmojiInputExample/src/com/demondevelopers/emoji/input/example/MainActivity.java
+++ EmojiInputExample/src/com/demondevelopers/emoji/input/example/MainActivity.java
@@ -1,9 +1,16 @@
 package com.demondevelopers.emoji.input.example;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.text.Editable;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.demondevelopers.emoji.input.lib.EmojiFragment;
 import com.demondevelopers.emoji.input.lib.EmojiPanelFragment;
@@ -14,7 +21,8 @@ public class MainActivity extends FragmentActivity
 	private EmojiPanelFragment mEmojiPanel;
 	
 	private EditText mInput;
-	
+    private FrameLayout mEmojiContainer;
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -28,7 +36,8 @@ public class MainActivity extends FragmentActivity
 		//mEmojiPanel = EmojiPanelFragment.initFragment(this, savedInstanceState, R.id.emoji_panel_container);
 		
 		//mEmojiPanel.initEditText((EditText)findViewById(R.id.input));
-		
+        mEmojiContainer = (FrameLayout) findViewById(R.id.emoji_panel_container);
+
 		if(savedInstanceState == null){
 			EmojiFragment frag = new EmojiFragment();
 			frag.setListener(mListener);
@@ -36,32 +45,67 @@ public class MainActivity extends FragmentActivity
 				.add(R.id.emoji_panel_container, frag)
 				.commit();
 		}
+        hideEmoji();
+        mInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_NEXT) {
+                    showEmoji();
+                    hideKeyboard();
+                    handled = true;
+                }
+                return handled;
+            }
+        });
 	}
-	
+
+    private void showEmoji(){
+        mEmojiContainer.setVisibility(View.VISIBLE);
+    }
+
+    private void hideEmoji(){
+        mEmojiContainer.setVisibility(View.GONE);
+    }
+
+    private void hideKeyboard(){
+        InputMethodManager imm = (InputMethodManager)getSystemService(
+          Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(mInput.getWindowToken(), 0);
+    }
+
+    private void showKeyboard(){
+
+    }
+
 	private EmojiFragment.Listener mListener = new EmojiFragment.Listener()
 	{
 		@Override
 		public void onInsertEmoji(CharSequence emojiSequence)
 		{
 			Editable editable = mInput.getText();
-			editable.append(emojiSequence);
+			editable.replace(mInput.getSelectionStart(), mInput.getSelectionEnd(), emojiSequence);
 		}
 		
 		@Override
 		public void onBackspace()
 		{
 			Editable editable = mInput.getText();
-			int len = editable.length();
-			if(len > 0){
-				int count = 1;
-				if(len > 1){
-					int codePoint = Character.codePointAt(editable, len - 2);
-					if(Character.charCount(codePoint) == 2){
-						count = 2;
-					}
-				}
-				editable.delete(len - count, len);
-			}
+			int selectionLength = mInput.getSelectionEnd() - mInput.getSelectionStart();
+            int start;
+            if (selectionLength > 0) {
+                start = mInput.getSelectionStart();
+            } else {
+                int count = 1;
+                if (mInput.getSelectionStart() > 1) {
+                    int codePoint = Character.codePointAt(editable, mInput.getSelectionStart() - 2);
+                    if (Character.charCount(codePoint) == 2) {
+                        count = 2;
+                    }
+                }
+                start = Math.max(0, mInput.getSelectionStart() - count);
+            }
+            editable.replace(start, mInput.getSelectionEnd(), "");
 		}
 	};
 }
